4f2cc892639faa1be9fc84317eba3d3ab0b9b40f
Ehugbo <thankgodehugbo@gmail.com>
2022-10-26 11:54:26 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * cap_string - Capitalizes all words of a string.
@@ -14,7 +14,7 @@ char *cap_string(char *str)
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
-		
+
 		if (str[index - 1] == ' ' ||
 			str[index - 1] == '\t' ||
 			str[index - 1] == '\n' ||
@@ -30,7 +30,7 @@ char *cap_string(char *str)
 			str[index - 1] == '}' ||
 			index == 0)
 			str[index] -= 32;
-		
+
 		index++;
 	}
 
